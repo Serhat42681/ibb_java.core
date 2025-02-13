236c073ed9229b175c5e268d35201c344e2a963f
Serhat TEKİN <sabriserhat42@gmail.com>
2025-02-13 20:02:57 +0300

    pom.xml dependencies

--- src/main/java/com/serhattekin/part_1/_01_JavaTypes.java
+++ src/main/java/com/serhattekin/part_1/_01_JavaTypes.java
@@ -1,11 +1,11 @@
 package com.serhattekin.part_1;
 
 
-public class _01_JavaTypes {
+public class _01_JavaTypes
+{
 
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+//        sout-- > tab
         System.out.println("Serhat Tekin");
     }
 }
