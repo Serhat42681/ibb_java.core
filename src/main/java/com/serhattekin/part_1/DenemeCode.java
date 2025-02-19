9a5b9f9d0c62a63e19f767cc9ba8f9f572e5f892
Serhat TEKİN <sabriserhat42@gmail.com>
2025-02-20 01:20:49 +0300

    pom.xml dependencies

---
+++ src/main/java/com/serhattekin/part_1/DenemeCode.java
@@ -0,0 +1,21 @@
+package com.serhattekin.part_1;
+
+import java.io.Closeable;
+import java.util.Scanner;
+
+public class DenemeCode {
+
+    public static void main(String[] args) {
+        String name,surname;
+        Scanner klavye= new Scanner(System.in);
+        System.out.println("Lütfen Adınızı giriniz");
+        name=klavye.nextLine();
+        System.out.println("Soyadınızı giriniz");
+        surname=klavye.nextLine();
+        System.out.printf("Adınız: " + name +  " Soyadınız; S" + surname);
+
+        //kullanmadığımız scanner nesnesini kapatalım .
+        klavye.close();
+
+    }
+}
