e02ab76ce32e5a98be5630c48927c09516ca9609
Serhat TEKİN <sabriserhat42@gmail.com>
2025-02-22 01:28:05 +0300

    pom.xml dependencies

---
+++ src/main/java/com/serhattekin/part_1/SwichCase.java
@@ -0,0 +1,17 @@
+package com.serhattekin.part_1;
+
+public class SwichCase {
+    public static void main(String[] args) {
+
+
+    int number=2;
+    switch(number){
+        case 1:
+            System.out.println("sayi 1");
+            break;
+        case 2:
+            System.out.printf("Sayi 2");
+    }
+
+    }
+}
