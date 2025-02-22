dd68275f8038a3ea5fa1bac382a73fb39574e455
Serhat TEKİN <sabriserhat42@gmail.com>
2025-02-22 19:56:18 +0300

    Swich Case

--- src/main/java/com/serhattekin/part_1/SwichCase.java
+++ src/main/java/com/serhattekin/part_1/SwichCase.java
@@ -1,18 +1,25 @@
 package com.serhattekin.part_1;
 
+import java.util.Scanner;
+
 public class SwichCase {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
-    int number=2;
-    switch(number){
-        case 1:
-            System.out.println("sayi 1");
-            break;
-        case 2:
-            System.out.printf("Sayi 2");
+        System.out.print("Bir sayı girin: ");
+        int number = scanner.nextInt(); // Kullanıcıdan giriş al
 
-    }
+        switch (number) {
+            case 1:
+                System.out.println("sayi 1");
+                break;
+            case 2:
+                System.out.printf("Sayi 2");
+            default:
+                System.out.printf("geçersiz sayı");
 
+        }
+scanner.close();
     }
 }
