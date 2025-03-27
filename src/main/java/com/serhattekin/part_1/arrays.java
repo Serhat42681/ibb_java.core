84ffa5c654e96f82b552a590b38e45ad15c0b38c
Serhat TEKİN <sabriserhat42@gmail.com>
2025-03-27 23:45:57 +0300

    Switch Case

--- src/main/java/com/serhattekin/part_1/arrays.java
+++ src/main/java/com/serhattekin/part_1/arrays.java
@@ -1,19 +1,30 @@
 package com.serhattekin.part_1;
 
 public class arrays {
-    static String[] city = new String[5];
-    public static void arrayMethod1() throws ArrayIndexOutOfBoundsException {
 
+    public static String[] arrayMethod1() throws ArrayIndexOutOfBoundsException {
+        String[] city = new String[2];
         city[0] = "Aksaray";
         city[1] = "Kahramanmaraş";
+
+
+        return city;
+    }
+    public static void arrayMethod2(){
+        String[] city = arrayMethod1();
+        for(String temp : city)
+        {
+            System.out.println(temp);
+        }
+
 
     }
 
     public static void main(String[] args) {
-        // Metottan gelen diziyi al
-        arrayMethod1();
 
-        // Dizinin elemanını yazdır
-        System.out.println(city[0]); // Çıktı: Kahramanmaraş
+        arrayMethod1();
+        arrayMethod2();
+
+
     }
 }
